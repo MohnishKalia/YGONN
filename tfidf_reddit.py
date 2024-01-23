54bb5ebb5218afd6b0fae53a20a10811d94acacd
Mohnish Kalia <mohnish.kalia@gmail.com>
2024-01-23 00:56:25 -0600

    Utilize some of tfidf

--- tfidf_reddit.py
+++ tfidf_reddit.py
@@ -8,13 +8,20 @@ if __name__ == '__main__':
     freeze_support()
 
     X_train, feature_names = load_dataset(verbose=True)
-    
-    cards = get_cdb_cards()
-    matches = dedupe_card_names(cards, feature_names.tolist())
 
-    # list(zip(feature_names, matches))
-    poss_matches = list(filter(lambda m: m[1], zip(feature_names, matches)))
-    # print(len(poss_matches))
+    features_sums = X_train.sum(axis=0)
+    features = sorted(list(zip(feature_names, features_sums.A1)), key=lambda kv: -kv[1])
+
+    with open('./google-10000-english-usa.txt', 'r') as f: 
+        common_words = [s.strip() for s in f.readlines()]
+
+    ygo_features = list(filter(lambda kv: (kv[1] >= 0.15) and (kv[0].strip() not in common_words), features))
+    ygo_feature_names = [cn for cn,_ in ygo_features]
+
+    cards = get_cdb_cards()
+    matches = dedupe_card_names(cards, ygo_feature_names)
+
+    poss_matches = list(filter(lambda m: m[1], zip(ygo_feature_names, matches)))
 
     with open("card_matches.json", 'w') as f:
         json.dump(poss_matches, f)
